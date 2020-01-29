9e1b2bea06fe8a586c31aea91d668b3e16f5685a
Maksim Kubrak <web@maksimkubrak.ru>
2020-01-29 15:25:59 +0300

    Create .description.php

---
+++ .description.php
@@ -0,0 +1,18 @@
+<?
+/*
+    Bitrix Offal Userdata v1.0 - https://github.com/max-kmb/offal
+    Быстрая очистка 1С-Битрикс
+
+    (c) 2020 Максим Кубрак - https://maksimkubrak.ru
+    web@maksimkubrak.ru
+*/
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+$arDescription = array(
+    'NAME' => 'Bitrix Offal Userdata',
+    'DESCRIPTION' => 'Структура инфоблоков',
+    'GROUP' => array('ID' => 'admin_content'),
+    'AI_ONLY' => true
+);
+?>
